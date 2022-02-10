b772704576c9bae0eb36e9b2c58b301a75cda8e4
sajibsarker-1104003 <sajibpranto600@gmail.com>
2022-02-11 02:21:37 +0600

    Create a MySQL Table Using MySQLi

--- demo.php
+++ demo.php
@@ -2,20 +2,28 @@
 $servername = "localhost";
 $username = "root";
 $password = "";
+$dbname = "myDB";
 
 // Create connection
-$conn = new mysqli($servername, $username, $password);
+$conn = new mysqli($servername, $username, $password, $dbname);
 // Check connection
 if ($conn->connect_error) {
   die("Connection failed: " . $conn->connect_error);
 }
 
-// Create database
-$sql = "CREATE DATABASE myDB";
+// sql to create table
+$sql = "CREATE TABLE Userinterface (
+id INT(6) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+firstname VARCHAR(30) NOT NULL,
+lastname VARCHAR(30) NOT NULL,
+email VARCHAR(50),
+reg_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
+)";
+
 if ($conn->query($sql) === TRUE) {
-  echo "Database created successfully";
+  echo "Table UserInterface created successfully";
 } else {
-  echo "Error creating database: " . $conn->error;
+  echo "Error creating table: " . $conn->error;
 }
 
 $conn->close();
