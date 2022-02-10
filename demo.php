06d7826ca2bb224f27ad2e2a32a71edd5fd22d67
sajibsarker-1104003 <sajibpranto600@gmail.com>
2022-02-11 02:25:03 +0600

    Insert Data Into MySQL Using MySQLi

--- demo.php
+++ demo.php
@@ -11,19 +11,13 @@ if ($conn->connect_error) {
   die("Connection failed: " . $conn->connect_error);
 }
 
-// sql to create table
-$sql = "CREATE TABLE Userinterface (
-id INT(6) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
-firstname VARCHAR(30) NOT NULL,
-lastname VARCHAR(30) NOT NULL,
-email VARCHAR(50),
-reg_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
-)";
+$sql = "INSERT INTO userinterface (firstname, lastname, email)
+VALUES ('John', 'Doe', 'john@example.com')";
 
 if ($conn->query($sql) === TRUE) {
-  echo "Table UserInterface created successfully";
+  echo "New record created successfully";
 } else {
-  echo "Error creating table: " . $conn->error;
+  echo "Error: " . $sql . "<br>" . $conn->error;
 }
 
 $conn->close();
