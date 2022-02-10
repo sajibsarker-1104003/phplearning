d821da86257b928199c21c94670cf40dbe06e187
sajibsarker-1104003 <sajibpranto600@gmail.com>
2022-02-11 02:11:36 +0600

    Create a MySQL Database Using MySQLi

--- demo.php
+++ demo.php
@@ -5,10 +5,18 @@ $password = "";
 
 // Create connection
 $conn = new mysqli($servername, $username, $password);
-
 // Check connection
 if ($conn->connect_error) {
   die("Connection failed: " . $conn->connect_error);
 }
-echo "Connected successfully";
+
+// Create database
+$sql = "CREATE DATABASE myDB";
+if ($conn->query($sql) === TRUE) {
+  echo "Database created successfully";
+} else {
+  echo "Error creating database: " . $conn->error;
+}
+
+$conn->close();
 ?>
